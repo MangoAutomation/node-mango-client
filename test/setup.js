018f744e42f659f6345403395df7af4e710c82f2
Jared Wiltshire <jazdw@users.noreply.github.com>
2017-05-11 16:46:04 -0600

    Ability to set options from command line

--- test/setup.js
+++ test/setup.js
@@ -2,15 +2,31 @@
  * Created by Jared on 5/11/2017.
  */
 
-let config;
+const path = require('path');
+
+const config = {username: 'admin', password: 'admin'};
+
+const commandLineArgs = require('command-line-args');
+const optionDefinitions = [
+    { name: 'config-file', alias: 'c', type: String },
+    { name: 'username', alias: 'u', type: String },
+    { name: 'password', alias: 'p', type: String },
+    { name: 'host', alias: 'h', type: String },
+    { name: 'port', type: Number  },
+    { name: 'protocol', type: String },
+    { name: 'reject-unauthorized', type: Boolean }
+];
+const commandLineOptions = commandLineArgs(optionDefinitions);
+
+const configPath = path.resolve(commandLineOptions['config-file'] || 'config.json');
+console.log(configPath);
 try {
-    config = require('./config.json');
+    let configFileOptions = require(configPath);
+    Object.assign(config, configFileOptions);
 } catch (e) {
-    config = {};
 }
 
-config.username = config.username || 'admin';
-config.password = config.password || 'admin';
+Object.assign(config, commandLineOptions);
 
 global.chai = require('chai');
 global.assert = chai.assert;
