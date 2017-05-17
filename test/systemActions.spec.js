65a8c72cc18ab8666e7b20239af7fc814dc1c8f4
terrypacker <username@users.noreply.github.com>
2017-05-16 15:10:47 -1000

    Adding system actions spec template for learning  the tests.

---
+++ test/systemActions.spec.js
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2017 Infinite Automation Systems Inc.
+ * http://infiniteautomation.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const config = require('./setup');
+
+describe('System Action Endpoints', () => {
+    before('Login', config.login);
+
+    it('Lists available actions', () => {
+      client.restRequest({
+          path: '/rest/v2/actions',
+          method: 'GET',
+          //data: {object}
+      }).then(response => {
+        console.log(`The available actions are '${response.data}'`);
+      });
+    });
+
+    it('Kick off purgeAllPointValues action', () => {
+      client.restRequest({
+          path: '/rest/v2/actions/purgeAllPointValues',
+          method: 'PUT',
+          data: {'test': 12}
+      }).then(response => {
+        console.log(response);
+      });
+    });
+
+});
