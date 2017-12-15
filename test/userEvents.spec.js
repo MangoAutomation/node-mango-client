e0ab12ca4841a3652738d73d1b0b24dcbedf7330
terrypacker <terrypacker@users.noreply.github.com>
2017-12-15 10:17:22 -1000

    Fixing User Event test

--- test/userEvents.spec.js
+++ test/userEvents.spec.js
@@ -19,9 +19,29 @@ const config = require('./setup');
 
 describe('User Event query tests', function(){
     before('Login', config.login);
-    before('Delay for login event to make it into the database', function(){
-        return delay(1000);
+
+    before('Insert a User Event', function(){
+        return client.restRequest({
+            path: '/rest/v2/example/raise-missing-event',
+            method: 'POST',
+            data: {
+                event: {
+                    typeName: "SYSTEM",
+                    systemEventType: 'Test event',
+                    ref1: -1,
+                },
+                level: 'URGENT',
+                message: 'testing',
+                context: {
+                    'value': "Testing"
+                }
+            }
+        }).then(response => {
+          //console.log(response.data);
+        });
     });
+
+
 
     it('Describe event query', () => {
       return client.restRequest({
@@ -33,13 +53,15 @@ describe('User Event query tests', function(){
     });
 
 
-    it('Simple event query', () => {
+    it('Query inserted event', () => {
       return client.restRequest({
-          path: '/rest/v2/user-events?limit(1)',
+          path: '/rest/v2/user-events?sort(-activeTimestamp)&limit(1)',
           method: 'GET'
       }).then(response => {
-        assert.isAbove(response.data.total, 0);
-        assert.equal(response.data.items.length, 1);
+          assert.isAbove(response.data.total, 0);
+          assert.equal(response.data.items.length, 1);
+          assert.equal(response.data.items[0].eventType.eventType, 'SYSTEM');
+          assert.equal(response.data.items[0].eventType.eventSubtype, 'Test event');
       });
     });
 
