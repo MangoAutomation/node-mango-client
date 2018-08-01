66316bab8f11c380ae007ad874a6b202bf9d8365
Phillip <puckfist@gmail.com>
2018-08-01 11:40:19 -0600

    refer to xid instead of id, per infiniteautomation/ma-core-public#1306

--- test/publisher.spec.js
+++ test/publisher.spec.js
@@ -130,7 +130,7 @@ describe('Publisher service', () => {
           parameterName : "Meter 3 - Power Factor A",
           includeTimestamp : true,
           modelType : "PUB-POINT-HTTP_SENDER",
-          dataPointId : global.dp.id
+          dataPointId : global.dp.xid
         } ],
         publishType : "ALL",
         cacheWarningSize : 100,
