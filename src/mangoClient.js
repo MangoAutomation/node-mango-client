e4ab8ba191614b691da4c47f5e4c1752e65ed9ee
Jared Wiltshire <jazdw@users.noreply.github.com>
2018-08-16 15:48:31 -0600

    Add port to host header if non standard port

--- src/mangoClient.js
+++ src/mangoClient.js
@@ -127,8 +127,16 @@ class MangoClient {
 
     restRequest(optionsArg) {
         let requestPromise = new Promise((resolve, reject) => {
+            let hostname = optionsArg.hostname;
+            if (!hostname) {
+                hostname = this.options.host;
+                if (!(this.options.protocol === 'http' && this.options.port !== 80 || this.options.protocol === 'https' && this.options.port !== 443)) {
+                    hostname += ':' + this.options.port;
+                }
+            }
+            
             const options = {
-                hostname: optionsArg.hostname || this.options.host,
+                hostname,
                 path : optionsArg.path,
                 agent: this.agent,
                 method : optionsArg.method || 'GET',
