eaf34a3e2d9459ff68658c919eb1f5376724b065
terrypacker <terrypacker@users.noreply.github.com>
2020-02-13 08:38:59 -1000

    Fix default for data source eventAlarmLevels

--- src/dataSource.js
+++ src/dataSource.js
@@ -1,18 +1,18 @@
 /**
  * Copyright 2017 Infinite Automation Systems Inc.
  * http://infiniteautomation.com/
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 const uuid = require('uuid/v4');
@@ -33,7 +33,7 @@ function dataSourceFactory(client) {
                 cronPattern: '',
                 pollPeriod: { periods: 5, type: 'SECONDS' },
                 purgeSettings: { override: false, frequency: { periods: 1, type: 'YEARS' } },
-                alarmLevels: { POLL_ABORTED: 'URGENT' },
+                eventAlarmLevels: [],
                 editPermission: null
             };
         }
